87588076b44c533b1b310d29b9a966c690c8619c
sanya25aug <89021784+sanya25aug@users.noreply.github.com>
2021-08-18 16:40:31 +0530

    Update BFS.cpp

--- BFS.cpp
+++ BFS.cpp
@@ -6,25 +6,29 @@
 using namespace std;
 
 template <typename T>
-class Graph
-{
-    map<T, list<T>> adjlist;
+class Graph{
+    // map to store the graph as adjacency list
+    // key-> node, value-> list of nodes connected to the key(node)
+    map<T, list<T>> adjList;
 
 public:
+    // add edge will add the edge from one node to another
+    // by default bidirectional edge is true, can be changed to false in case of directed graph
     void addEdge(T u, T v, bool bidirectionalEdge = true)
     {
-        adjlist[u].push_back(v);
+        adjList[u].push_back(v);
         if (bidirectionalEdge)
         {
-            adjlist[v].push_back(u);
+            adjList[v].push_back(u);
         }
     }
 
+    // function to print the adjaceny list for each node
     void printAdjlist()
     {
 
         //iterate over the map
-        for (auto i : adjlist)
+        for (auto i : adjList)
         {
             cout << i.first << "->";
 
@@ -36,30 +40,47 @@ public:
         }
     }
 
-    void bfs(T src)
-    {
+    // bfs function
+    void bfs(T src, T dest) {
         queue<T> q;
-        map<T, bool> visited;
-        q.push(src);
-        visited[src] = true;
+		map<T,int> dist;
+		map<T,T> parent;
 
-        while (!q.empty())
-        {
-            T node = q.front();
-            cout << node << " ";
-            q.pop();
+		for(auto i:adjList){
+			dist[i.first] = INT_MAX;
+		}
 
-            // For the neighbours of current node find nodes that are not visited
-            for (int neighbour : adjlist[node])
-            {
-                if (!visited[neighbour])
-                {
-                    q.push(neighbour);
-                    visited[neighbour] = true;
-                }
-            }
-        }
-    }
+		q.push(src);
+		dist[src] = 0;
+		parent[src] = src;
+
+		while(!q.empty()){
+
+			T node = q.front();
+			cout<<node<<" ";
+			q.pop();
+
+			// For the neigbours of the current node, find out the nodes which are not visited
+			for(int neigbour :adjList[node]){
+				if(dist[neigbour]==INT_MAX){
+					q.push(neigbour);
+					dist[neigbour] = dist[node]  + 1;
+					parent[neigbour] = node;
+				}
+			}
+		}
+        cout<<endl;
+
+		//Print the distance of destination node from source node
+		for(auto i:adjList){
+		        if(i.first==dest){
+				T node = i.first;
+				cout<<"Dist of "<<node<<" from "<<src<<" is "<<dist[node]<<endl;
+		        }
+		}
+
+	}
+
 };
 
 int main()
@@ -79,11 +100,13 @@ int main()
         cin >> u >> v;
         g.addEdge(u, v);
     }
-    int src;
+    int src, dest;
     cout<<"Enter source node: ";
     cin>>src;
-    cout<<"BFS: ";
-    g.bfs(src);
+    cout<<"Enter destination node: ";
+    cin>>dest;
+    cout<<"BFS traversal: ";
+    g.bfs(src, dest);
 
     return 0;
 }
